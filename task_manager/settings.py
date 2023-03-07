9b93621281c9fcb0bafbe1f0847d8c86f820699c
Sergey Kim <sergkim7@gmail.com>
2023-03-07 10:51:30 +0300

    add localization

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -131,3 +131,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LANGUAGE_CODE = 'ru'
+
+LOCALE_PATHS = (
+    './locale/',
+)
