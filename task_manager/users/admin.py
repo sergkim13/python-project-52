c35ef1c837d6a398717b8b5f6a59333642549071
Sergey Kim <sergkim7@gmail.com>
2023-03-25 17:10:43 +0300

    add User to admin

--- task_manager/users/admin.py
+++ task_manager/users/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from .models import User
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'first_name',
+                    'last_name', 'email', 'date_joined')
+    search_fields = ('username', 'first_name', 'last_name', 'email')
+    list_filter = (('date_joined'),)
